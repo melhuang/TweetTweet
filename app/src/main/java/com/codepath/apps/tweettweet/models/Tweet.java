a0200aa10afe270fd8becd135bc31033978957f1
Melissa Huang <melissahuang10@gmail.com>
2016-08-04 21:56:33 -0700

    Initial commit with base functionality

---
+++ app/src/main/java/com/codepath/apps/tweettweet/models/Tweet.java
@@ -0,0 +1,65 @@
+package com.codepath.apps.tweettweet.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+/**
+ * Created by melissahuang on 8/4/16.
+ */
+public class Tweet {
+
+    private String body;
+    private long uid; // unique id
+    private User user;
+    private String createdAt;
+
+    public String getBody() {
+        return body;
+    }
+
+    public long getUid() {
+        return uid;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public static Tweet fromJSON(JSONObject jsonObject) {
+        Tweet tweet = new Tweet();
+        try {
+            tweet.body = jsonObject.getString("text");
+            tweet.uid = jsonObject.getLong("id");
+            tweet.createdAt = jsonObject.getString("created_at");
+            tweet.user = User.fromJSON(jsonObject.getJSONObject("user"));
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+//        tweet.user =
+        return tweet;
+    }
+
+    public static ArrayList<Tweet> fromJSONArray(JSONArray jsonArray) {
+        ArrayList<Tweet> tweets = new ArrayList<>();
+        for (int i = 0; i < jsonArray.length(); i++) {
+            try {
+                JSONObject tweetJson = jsonArray.getJSONObject(i);
+                Tweet tweet = Tweet.fromJSON(tweetJson);
+                if (tweet != null) {
+                    tweets.add(tweet);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        return tweets;
+    }
+}
