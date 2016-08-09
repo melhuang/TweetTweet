f43b4bc81938870e5ee9637e8430e51bf4e086ea
Melissa Huang <melissahuang10@gmail.com>
2016-08-08 19:51:37 -0700

    Move basic properties to fragment

--- app/src/main/java/com/codepath/apps/tweettweet/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/tweettweet/TimelineActivity.java
@@ -3,46 +3,33 @@ package com.codepath.apps.tweettweet;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 
+import com.codepath.apps.tweettweet.fragments.TweetsListFragment;
 import com.codepath.apps.tweettweet.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import java.util.ArrayList;
-
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity implements ComposeDialogFragment.ComposeDialogFragmentListener {
 
     private TwitterClient client;
-    private TweetsAdapter aTweets;
-    private ArrayList<Tweet> tweets;
-    private RecyclerView rvTweets;
+    private TweetsListFragment fragmentTweetsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        rvTweets = (RecyclerView) findViewById(R.id.rvTweets);
-        tweets = new ArrayList<>();
-        aTweets = new TweetsAdapter(this, tweets);
-        rvTweets.setAdapter(aTweets);
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        rvTweets.setLayoutManager(linearLayoutManager);
-        rvTweets.setHasFixedSize(true);
-        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
-            @Override
-            public void onLoadMore(int page, int totalItemsCount) {
-                populateTimeline();
-            }
-        });
+
         client = TwitterApplication.getRestClient();
+        // if savedInstanceState != null, the fragment is already in memory
+        if (savedInstanceState == null) {
+            fragmentTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
+        }
         populateTimeline();
     }
 
@@ -56,15 +43,14 @@ public class TimelineActivity extends AppCompatActivity implements ComposeDialog
 //    }
 
     private void populateTimeline() {
-        long fetchMoreStart = tweets.size() > 0 ? tweets.get(tweets.size() - 1).getUid() : -1;
+        long fetchMoreStart = -1;
+//        long fetchMoreStart = tweets.size() > 0 ? tweets.get(tweets.size() - 1).getUid() : -1;
         client.getHomeTimeline(new JsonHttpResponseHandler() {
             // on success
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONArray json) {
-                int start = aTweets.getItemCount();
-                tweets.addAll(Tweet.fromJSONArray(json));
-                aTweets.notifyItemRangeInserted(start, aTweets.getItemCount() - 1);
+                fragmentTweetsList.addAll(Tweet.fromJSONArray(json));
             }
 
             // on failure
@@ -84,20 +70,20 @@ public class TimelineActivity extends AppCompatActivity implements ComposeDialog
     // ComposeDialogFragmentListener
     @Override
     public void onSaveTweet(String body) {
-        client.composeTweet(new JsonHttpResponseHandler() {
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONObject json) {
-                // fetch most recent tweets
-                Tweet newTweet = Tweet.fromJSON(json);
-                tweets.add(0, newTweet);
-                aTweets.notifyItemInserted(0);
-            }
-
-            // on failure
-            @Override
-            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
-                Log.d("DEBUG", errorResponse.toString());
-            }
-        }, body);
+//        client.composeTweet(new JsonHttpResponseHandler() {
+//            @Override
+//            public void onSuccess(int statusCode, Header[] headers, JSONObject json) {
+//                // fetch most recent tweets
+//                Tweet newTweet = Tweet.fromJSON(json);
+//                tweets.add(0, newTweet);
+//                aTweets.notifyItemInserted(0);
+//            }
+//
+//            // on failure
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+//                Log.d("DEBUG", errorResponse.toString());
+//            }
+//        }, body);
     }
 }
---
+++ app/src/main/java/com/codepath/apps/tweettweet/fragments/TweetsListFragment.java
@@ -0,0 +1,62 @@
+package com.codepath.apps.tweettweet.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.codepath.apps.tweettweet.EndlessRecyclerViewScrollListener;
+import com.codepath.apps.tweettweet.R;
+import com.codepath.apps.tweettweet.TweetsAdapter;
+import com.codepath.apps.tweettweet.models.Tweet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by melissahuang on 8/6/16.
+ */
+public class TweetsListFragment extends Fragment {
+
+    private TweetsAdapter aTweets;
+    private ArrayList<Tweet> tweets;
+    private RecyclerView rvTweets;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tweets_list, container, false);
+        rvTweets = (RecyclerView) v.findViewById(R.id.rvTweets);
+        rvTweets.setAdapter(aTweets);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        rvTweets.setLayoutManager(linearLayoutManager);
+        rvTweets.setHasFixedSize(true);
+        rvTweets.addOnScrollListener(new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount) {
+//                populateTimeline();
+            }
+        });
+
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        tweets = new ArrayList<>();
+        aTweets = new TweetsAdapter(getActivity(), tweets);
+        super.onCreate(savedInstanceState);
+    }
+
+    // Fragment accessor methods
+    public void addAll(List<Tweet> tweets) {
+        int start = aTweets.getItemCount();
+        this.tweets.addAll(tweets);
+        aTweets.notifyItemRangeInserted(start, aTweets.getItemCount() - 1);
+    }
+
+}
